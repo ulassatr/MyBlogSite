dcaba58755d073f38258cec322b5ad039fc1936f
ulassatr <ulassatr@gmail.com>
2017-12-24 04:59:45 +0300

    MyBlogSite v0.8.3

---
+++ Blog02.WebApp/Controllers/CommentController.cs
@@ -0,0 +1,121 @@
+﻿using Blog02.WebApp.Models;
+using MyBlogSite.BusinessLayer;
+using MyBlogSite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog02.WebApp.Controllers
+{
+    public class CommentController : Controller
+    {
+        private NoteManager noteManager = new NoteManager();
+        private CommentManager commentManager = new CommentManager();
+
+        public ActionResult ShowNoteComments(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Note note = noteManager.Find(x => x.Id == id);
+
+            if (note == null)
+            {
+
+                return HttpNotFound();
+            }
+
+
+            return PartialView("_PartialComments",note.Comments);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id,string text)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = commentManager.Find(x => x.Id == id);
+
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            comment.Text = text;
+
+            if (commentManager.Update(comment)>0)
+            {
+                return Json(new { result = true },JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = commentManager.Find(x => x.Id == id);
+
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+
+            if (commentManager.Delete(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(Comment comment,int? noteid)
+        {
+            ModelState.Remove("CreatedOn");
+            ModelState.Remove("ModifiedOn");
+            ModelState.Remove("ModifiedUsername");
+
+            if (ModelState.IsValid)
+            {
+                if (noteid == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Note note = noteManager.Find(x => x.Id == noteid);
+
+                if (note == null)
+                {
+                    return new HttpNotFoundResult();
+                }
+                comment.Note = note;
+                comment.Owner = CurrentSession.User;
+
+
+                if (commentManager.Insert(comment) > 0)
+                {
+                    return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+                }
+                            
+                
+            }
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ MyBlogSite.BusinessLayer/CommentManager.cs
@@ -0,0 +1,14 @@
+﻿using MyBlogSite.BusinessLayer.Abstract;
+using MyBlogSite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogSite.BusinessLayer
+{
+    public class CommentManager :ManagerBase<Comment>
+    {
+    }
+}
